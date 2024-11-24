ee64e2ac7dcf700a974130fd0748cf4731639932
gurungDev <ngrgurung890@gmail.com>
2024-11-24 11:17:36 +0545

    argocd try take 2

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "helm_release" "argocd" {
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "5.12.0"  
-  values = [file("values/argocd.yaml")]
+  values = [file("./argocd.yaml")]
 
 }
 
